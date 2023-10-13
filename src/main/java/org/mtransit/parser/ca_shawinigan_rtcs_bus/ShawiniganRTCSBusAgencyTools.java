68af7421b615989e29a3fe0ad74e0a7a118e508d
Mathieu Méa <mathieu.mea@gmail.com>
2023-10-13 16:07:37 -0400

    Compatibility with latest update

--- src/main/java/org/mtransit/parser/ca_shawinigan_rtcs_bus/ShawiniganRTCSBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_shawinigan_rtcs_bus/ShawiniganRTCSBusAgencyTools.java
@@ -1,8 +1,11 @@
 package org.mtransit.parser.ca_shawinigan_rtcs_bus;
 
+import static org.mtransit.commons.StringUtils.EMPTY;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.Letters;
 import org.mtransit.commons.RegexUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.mt.data.MAgency;
@@ -11,10 +14,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
-import static org.mtransit.commons.StringUtils.EMPTY;
-
 // https://donnees-shawinigan.opendata.arcgis.com/
-// https://jmap.shawinigan.ca/doc/photos/google_transit.zip
+// OLD: https://jmap.shawinigan.ca/doc/photos/google_transit.zip
 public class ShawiniganRTCSBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(@NotNull String[] args) {
@@ -54,7 +55,23 @@ public class ShawiniganRTCSBusAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
+	@Override
+	public @Nullable Long convertRouteIdFromShortNameNotSupported(@NotNull String routeShortName) {
+		switch (routeShortName) {
+		case "A":
+			return 1000L + Letters.A;
+		case "B":
+			return 1000L + Letters.B;
+		case "N":
+			return 1000L + Letters.N;
+		}
+		return super.convertRouteIdFromShortNameNotSupported(routeShortName);
+	}
+
 	private static final Pattern ENDS_W_COMMA_ = Pattern.compile("((?<=(\\w))(,)(.*)$)");
+
+	private static final Pattern EXPRESS_LETTER_ = Pattern.compile("((express)([a-z]))", Pattern.CASE_INSENSITIVE);
+	private static final String EXPRESS_LETTER_REPLACEMENT = "$2 $3";
 
 	@Override
 	public boolean defaultRouteLongNameEnabled() {
@@ -64,6 +81,8 @@ public class ShawiniganRTCSBusAgencyTools extends DefaultAgencyTools {
 	@NotNull
 	@Override
 	public String cleanRouteLongName(@NotNull String routeLongName) {
+		routeLongName = CleanUtils.toLowerCaseUpperCaseWords(getFirstLanguageNN(), routeLongName);
+		routeLongName = EXPRESS_LETTER_.matcher(routeLongName).replaceAll(EXPRESS_LETTER_REPLACEMENT);
 		routeLongName = ENDS_W_COMMA_.matcher(routeLongName).replaceAll(EMPTY);
 		routeLongName = CleanUtils.CLEAN_ET.matcher(routeLongName).replaceAll(CleanUtils.CLEAN_ET_REPLACEMENT);
 		routeLongName = CleanUtils.SAINT.matcher(routeLongName).replaceAll(CleanUtils.SAINT_REPLACEMENT);
